ad84e14fb13e931104ad3c1391c7d41e0f80e84d
arcba <alejandroreyc@yahoo.com.ar>
2020-10-26 16:17:26 -0300

    2 files modified, 1 added

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Articulo } from "../../models/articulo";
+import { ArticuloFamilia } from "../../models/articulo-familia";
+import { MockArticulosService } from "../../services/mock-articulos.service";
+import { MockArticulosFamiliasService } from "../../services/mock-articulos-familias.service";
+import { ArticulosService } from "../../services/articulos.service";
+import { ArticulosFamiliasService } from "../../services/articulos-familias.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ModalDialogService } from "../../services/modal-dialog.service";
 
 @Component({
-  selector: 'app-servicios',
-  templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  selector: "app-servicios",
+  templateUrl: "./servicios.component.html",
+  styleUrls: ["./servicios.component.css"]
 })
 export class ServiciosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+    
   }
-
+  ngOnInit() {}
 }
